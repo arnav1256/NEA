5bb9f47730622a8ef633d58da0cf2bfb16193df9
arnav1256 <92578817+arnav1256@users.noreply.github.com>
2023-03-23 10:09:17 +0000

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
-import requests
-import random
-import string
-import pickle
+from requests import get
+from random import randint, choice
+from string import ascii_letters, punctuation
+from pickle import load, dump
 import xml.etree.ElementTree as ET
 
 
@@ -10,7 +10,7 @@ class HashTable:
         self.size = size
         self.arr = [[] for _ in range(self.size)]
 
-    def _hash(self, key):
+    def __hash(self, key):
         x = 0x192873
         for c in key:
             x = ((1000003 * x) ^ ord(c)) & 0xffffffff
@@ -21,7 +21,7 @@ class HashTable:
 
     def __setitem__(self, key, val):
         exists = False
-        index = self._hash(key)
+        index = self.__hash(key)
         for i, v in enumerate(self.arr[index]):
             if len(v) == 2 and v[0] == key:
                 self.arr[index][i] = (key, val)
@@ -31,16 +31,16 @@ class HashTable:
             self.arr[index].append((key, val))
 
     def __getitem__(self, key):
-        index = self._hash(key)
+        index = self.__hash(key)
         for i in self.arr[index]:
             if i[0] == key:
                 return i[1]
 
-    # def __delitem__(self, key):
-    #     index = self._hash(key)
-    #     for i, v in enumerate(self.arr[index]):
-    #         if v[0] == key:
-    #             del self.arr[index][i]
+    def __delitem__(self, key):
+        index = self.__hash(key)
+        for i, v in enumerate(self.arr[index]):
+            if v[0] == key:
+                del self.arr[index][i]
 
 
 class Train:
@@ -77,10 +77,9 @@ class LinkedList:
         self.length += 1
 
     def pop(self, index):
-        i = 0
         if self.head is None:
             return
-        if i == index:
+        if index == 0:
             val = self.head.value
             self.head = self.head.next
             if self.head is None:
@@ -88,16 +87,16 @@ class LinkedList:
             self.length -= 1
             return val
         current_node = self.head
-        while current_node.next is not None:
-            i += 1
-            if i == index:
-                val = current_node.next.value
-                current_node.next = current_node.next.next
-                if current_node.next is None:
-                    self.tail = current_node
-                self.length -= 1
-                return val
+        for i in range(index - 1):
             current_node = current_node.next
+            if current_node is None:
+                return
+        val = current_node.next.value
+        current_node.next = current_node.next.next
+        if current_node.next is None:
+            self.tail = current_node
+        self.length -= 1
+        return val
 
     def __str__(self):
         result = []
@@ -107,49 +106,23 @@ class LinkedList:
             current_node = current_node.next
         return ' -> '.join(map(str, result))
 
-    # def prepend(self, value):
-    #     newNode = Node(value)
-    #     if self.head is None:
-    #         self.head = newNode
-    #         self.tail = newNode
-    #     else:
-    #         newNode.next = self.head
-    #         self.head = newNode
-    #     self.length += 1
+    def prepend(self, value):
+        new_node = LinkedListNode(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
 
-    # def insert_after(self, prev_node, value):
-    #     if not prev_node:
-    #         raise ValueError("Previous node must be valid")
-    #     new_node = Node(value)
-    #     new_node.next = prev_node.next
-    #     prev_node.next = new_node
-
-    # def search(self, value):
-    #     current_node = self.head
-    #     while current_node is not None:
-    #         if current_node.value == value:
-    #             return current_node
-    #         current_node = current_node.next
-    #     return None
-
-    # def remove(self, value):
-    #     if self.head is None:
-    #         return
-    #     if self.head.value == value:
-    #         self.head = self.head.next
-    #         if self.head is None:
-    #             self.tail = None
-    #         self.length -= 1
-    #         return
-    #     currentNode = self.head
-    #     while currentNode.next is not None:
-    #         if currentNode.next.value == value:
-    #             currentNode.next = currentNode.next.next
-    #             if currentNode.next is None:
-    #                 self.tail = currentNode
-    #             self.length -= 1
-    #             return
-    #         currentNode = currentNode.next
+    @staticmethod
+    def insert_after(prev_node, value):
+        if not prev_node:
+            raise ValueError("Previous node does not exist.")
+        new_node = LinkedListNode(value)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
 
 
 class User:
@@ -158,6 +131,15 @@ class User:
         self.password = pwdhash
         self.salt = salt
         self.journeys = []
+
+
+class Journey:
+    def __init__(self, src, dst):
+        self.source = src
+        self.destination = dst
+        self.bfs = traverse(graph, locations[src], locations[dst], "bfs")
+        self.dfs = traverse(graph, locations[src], locations[dst], "dfs")
+        self.djk = dijkstra(graph, locations[src], locations[dst])
 
 
 class Graph:
@@ -175,9 +157,6 @@ class Graph:
             return []
         else:
             return self.graph[node]
-
-    def get_vertices(self):
-        return list(self.graph.keys())
 
 
 class DijkstraNode:
@@ -199,12 +178,12 @@ def dijkstra(g, start, end):
         queue = queue[1:]
         if current_node.name == end:
             break
-        for neighbor, weight in g.get_neighbours(current_node.name):
+        for neighbour, weight in g.get_neighbours(current_node.name):
             new_distance = visited[current_node.name].dist + float(weight) + 0.38
-            if new_distance < visited[neighbor].dist:
-                visited[neighbor].dist = new_distance
-                visited[neighbor].prev = current_node
-                queue.append(visited[neighbor])
+            if new_distance < visited[neighbour].dist:
+                visited[neighbour].dist = new_distance
+                visited[neighbour].prev = current_node
+                queue.append(visited[neighbour])
     path = []
     node = visited[end]
     while node.name != start:
@@ -214,7 +193,7 @@ def dijkstra(g, start, end):
     path.reverse()
     for i in range(len(path)):
         path[i] = places[path[i]]
-    return path, visited[end].dist
+    return path, round_num(visited[end].dist)
 
 
 def traverse(g, start, end, algo="bfs"):
@@ -240,11 +219,11 @@ def traverse(g, start, end, algo="bfs"):
     return [], float('inf')
 
 
-def hash(key, salt, pepper):
+def hash(key, salt, pepr):
     key = [chr(ord(key[i]) ^ ord(salt[i])) for i in range(min(len(key), len(salt)))]
     a = 0x192873
     for c in key:
-        a = ((pepper * a) ^ ord(c)) & 0xffffffff
+        a = ((pepr * a) ^ ord(c)) & 0xffffffff
     a ^= len(key)
     if a == -1:
         a = -2
@@ -252,7 +231,7 @@ def hash(key, salt, pepper):
 
 
 def generate_salt(str_size, allowed_chars):
-    return ''.join(random.choice(allowed_chars) for _ in range(str_size))
+    return ''.join(choice(allowed_chars) for _ in range(str_size))
 
 
 def signup():
@@ -263,7 +242,7 @@ def signup():
     try:
         with open("login.txt", "rb") as f:
             try:
-                d = pickle.load(f)
+                d = load(f)
             except:
                 d = []
             for i in d:
@@ -272,7 +251,9 @@ def signup():
                     print("Username already exists")
     except:
         open("login.txt", "w").close()
+        d = []
     while (not confirm == password) or exists:
+        print("Username already exists or password input incorrectly. ")
         username = input("Enter your username: ")
         password = input("Enter your password: ")
         confirm = input("Enter your password again: ")
@@ -283,78 +264,74 @@ def signup():
         else:
             exists = False
     with open("login.txt", "wb") as g:
-        salt = generate_salt(len(password) + random.randint(0, 5), string.ascii_letters + string.punctuation)
+        salt = generate_salt(len(password) + randint(0, 10), ascii_letters + punctuation)
         user = User(username, str(hash(password, salt, pepper)), salt)
         d.append(user)
-        pickle.dump(d, g)
+        dump(d, g)
     print("Signed up successfully.")
 
 
 def login():
-    loggedIn = False
-    while not loggedIn:
+    logged_in = False
+    while not logged_in:
         username = input("Enter your username: ")
         password = input("Enter your password: ")
         try:
             with open("login.txt", "rb") as f:
-                d = pickle.load(f)
+                d = load(f)
         except FileNotFoundError:
             open("login.txt", "w").close()
             print("No Users.")
             return
-        except Exception as x:
-            print(x)
-            return
         for i in d:
             if i.username == username and int(i.password) == hash(password, i.salt, pepper):
                 print("Logged in successfully.")
-                loggedIn = True
+                logged_in = True
                 break
         else:
             print("Incorrect username or password.")
     with open("login.txt", "wb") as f:
-        pickle.dump(d, f)
+        dump(d, f)
     return username
 
 
 def save(username):
-    hasJourney = False
-    while not hasJourney:
+    has_journey = False
+    while not has_journey:
         source = input("Enter source station: ")
         destination = input("Enter destination station: ")
         if locations[source] is not None and locations[destination] is not None:
-            hasJourney = True
+            has_journey = True
         else:
             print("Incorrect station names.")
     with open("login.txt", "rb") as f:
-        d = pickle.load(f)
+        d = load(f)
     for i in d:
         if i.username == username:
-            i.journeys.append((source, destination, traverse(graph, locations[source], locations[destination], "bfs"),
-                               traverse(graph, locations[source], locations[destination], "dfs"),
-                               dijkstra(graph, locations[source], locations[destination])))
+            j = Journey(source, destination)
+            i.journeys.append(j)
     with open("login.txt", "wb") as f:
-        pickle.dump(d, f)
+        dump(d, f)
     print("Journey saved successfully.")
 
 
 def read(username):
     with open("login.txt", "rb") as f:
-        d = pickle.load(f)
+        d = load(f)
     for i in d:
         if i.username == username:
-            if i.journeys == []:
+            if not i.journeys:
                 print("No journeys saved.")
             for j in range(len(i.journeys)):
-                print(f"Journey {j + 1}: {i.journeys[j][0]} -> {i.journeys[j][1]}")
-                print(f"BFS Route: {' -> '.join(i.journeys[j][2][0])}")
-                print(f"Time Taken: {i.journeys[j][2][1]} mins")
-                print(f"DFS Route: {' -> '.join(i.journeys[j][3][0])}")
-                print(f"Time Taken: {i.journeys[j][3][1]} mins")
-                print(f"Dijkstra Route: {' -> '.join(i.journeys[j][4][0])}")
-                print(f"Time Taken: {i.journeys[j][4][1]} mins\n")
+                print(f"Journey {j + 1}: {i.journeys[j].source} -> {i.journeys[j].destination}")
+                print(f"BFS Route: {' -> '.join(i.journeys[j].bfs[0])}")
+                print(f"Time Taken: {i.journeys[j].bfs[1]} mins")
+                print(f"DFS Route: {' -> '.join(i.journeys[j].dfs[0])}")
+                print(f"Time Taken: {i.journeys[j].dfs[1]} mins")
+                print(f"Dijkstra Route: {' -> '.join(i.journeys[j].djk[0])}")
+                print(f"Time Taken: {i.journeys[j].djk[1]} mins\n")
     with open("login.txt", "wb") as f:
-        pickle.dump(d, f)
+        dump(d, f)
 
 
 def print_journey(journey):
@@ -370,13 +347,6 @@ def calc_min_run(n):
         n >>= 1
     return n + r
 
-# def insertion(arr, left, right):
-#     for i in range(left + 1, right + 1):
-#         j = i
-#         while j > left and arr[j] < arr[j - 1]:
-#             arr[j], arr[j - 1] = arr[j - 1], arr[j]
-#             j -= 1
-
 
 def insertion(array, start, end):
     for i in range(start + 1, end + 1):
@@ -391,12 +361,7 @@ def insertion(array, start, end):
 
 
 def binary_search(array, item, start, end):
-    if start == end:
-        if array[start] > item:
-            return start
-        else:
-            return start + 1
-    elif start > end:
+    if start > end:
         return start
     mid = (start + end) // 2
     if array[mid] < item:
@@ -413,14 +378,13 @@ def timsort(arr):
     for start in range(0, n, min_run):
         end = min(start + min_run - 1, n - 1)
         insertion(arr, start, end)
-    size = min_run
-    while size < n:
-        for left in range(0, n, 2 * size):
-            mid = min(n - 1, left + size - 1)
-            right = min((left + 2 * size - 1), (n - 1))
+    while min_run < n:
+        for left in range(0, n, 2 * min_run):
+            mid = min(n - 1, left + min_run - 1)
+            right = min((left + 2 * min_run - 1), (n - 1))
             if mid < right:
                 merge(arr, left, mid, right)
-        size = 2 * size
+        min_run = 2 * min_run
     return arr
 
 
@@ -445,10 +409,7 @@ def merge(arr, start, mid, end):
 
 def round_num(n, d=0):
     a = ((n * (10 ** d) + 0.5) // 1) / (10 ** d)
-    if d <= 0:
-        return int(a)
-    else:
-        return a
+    return int(a) if d <= 0 else a
 
 
 def arrivals(loc):
@@ -456,7 +417,7 @@ def arrivals(loc):
         if locations[loc] is None:
             print("Invalid Station name.")
             return
-        data = requests.get(f"https://api.tfl.gov.uk/StopPoint/{locations[loc]}/Arrivals?mode=tube")
+        data = get(f"https://api.tfl.gov.uk/StopPoint/{locations[loc]}/Arrivals?mode=tube")
         d = [v for v in data.json()]
         a = [(i["lineName"], i['platformName'].split()[0], i["timeToStation"], i["towards"]) for i in d]
         temp = []
@@ -490,30 +451,27 @@ def print_arrivals(data):
 
 
 def print_status():
-    data = requests.get("http://cloud.tfl.gov.uk/TrackerNet/LineStatus")
+    data = get("http://cloud.tfl.gov.uk/TrackerNet/LineStatus")
     tree = ET.ElementTree(ET.fromstring(data.content))
     li = [[w.attrib for w in v] for v in tree.getroot()]
     arr = [v.attrib["StatusDetails"] for v in tree.getroot()]
     for i in range(len(li)):
         print(li[i][1]["Name"])
         print(li[i][2]["Description"])
-        if arr[i] != "":
+        if arr[i]:
             print(arr[i])
         print()
 
 
 def api_journey(start, end):
-    #print(start, end)
-    data = requests.get(f"https://api.tfl.gov.uk/Journey/JourneyResults/{start}/to/{end}").json()
-    count = 1
-    for i in data["journeys"]:
-        print(f"Journey {count}:")
-        print(f"Duration: {i['duration']} mins")
-        print(f"Cost: £{i['fare']['totalCost'] / 100}")
-        for j in i["legs"]:
+    data = get(f"https://api.tfl.gov.uk/Journey/JourneyResults/{start}/to/{end}").json()
+    for idx, elem in enumerate(data["journeys"]):
+        print(f"Journey {idx + 1}:")
+        print(f"Duration: {elem['duration']} mins")
+        print(f"Cost: £{elem['fare']['totalCost'] / 100}")
+        for j in elem["legs"]:
             print(j["instruction"]["summary"])
         print()
-        count += 1
 
 
 def main():
@@ -537,7 +495,7 @@ def main():
         if i:
             for j in i:
                 graph.add_vertex(j[1])
-    data = requests.get("https://raw.githubusercontent.com/egkoppel/tube-timings/main/data.txt")
+    data = get("https://raw.githubusercontent.com/egkoppel/tube-timings/main/data.txt")
     li = [i.split() for i in data.text.split("\n")][:-1]
     for i in li:
         graph.add_edge(i[0], i[1], i[2])
@@ -547,7 +505,6 @@ def main():
             option = int(option)
         except:
             print("Invalid choice")
-            pass
         if option == 1:
             signup()
         elif option == 2:
@@ -559,7 +516,7 @@ def main():
                 elif choice == 2:
                     read(username)
         elif option == 3:
-            print("Choose a graph traversal algorithm (ranked by efficiency):\n\t1. TfL API\n\t2. Dijkstra\n\t3. BFS"
+            print("Choose a method (ranked by efficiency):\n\t1. TfL API\n\t2. Dijkstra\n\t3. BFS"
                   "\n\t4. DFS")
             choice = input()
             try:
@@ -597,30 +554,3 @@ if __name__ == '__main__':
         main()
     except Exception as e:
         print(e)
-
-# with open("thing.txt", "r") as f:
-#     a = [v[:-20] + "\n" for v in f.read().splitlines()]
-# with open("thing.txt", "w") as f:
-#     f.writelines(a)
-
-
-# print(traverse(graph, locations["Hammersmith (H&C Line)"], locations["Hammersmith (Dist&Picc Line)"], "bfs"))
-# print(traverse(graph, locations["Uxbridge"], locations["Cockfosters"], "bfs"))
-# print(traverse(graph, locations["Upminster"], locations["Chesham"], "bfs"))
-# print(traverse(graph, locations["Golders Green"], locations["Hammersmith (Dist&Picc Line)"], "bfs"))
-# print(traverse(graph, locations["Mill Hill East"], locations["Hammersmith (Dist&Picc Line)"], "bfs"))
-
-# t = HashTable(10)
-# t["march 6"] = 130
-# t["march 8"] = 20
-# t["march 17"] = 10
-# del t["march 6"]
-# print(t.arr)
-# print(t["march 6"])
-
-# li = LinkedList()
-# li.append(1)
-# li.append(5)
-# li.prepend("string")
-# li.remove(5)
-# print(li)
